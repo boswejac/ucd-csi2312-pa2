ae4edb338b75c76ce5d03ca8717dfbf2ab65385a
boswejac <jacob.boswell@umontana.edu>
2015-10-03 19:51:27 -0600

    All functions working

--- Cluster.cpp
+++ Cluster.cpp
@@ -4,206 +4,293 @@
 
 #include "Cluster.h"
 
-Clustering::Cluster::Cluster(const Clustering::Cluster &cluster) {
-    size = cluster.getSize();
-    points = cluster.getPtr();
-}
+namespace Clustering {
 
-Clustering::Cluster &Clustering::Cluster::operator=(const Clustering::Cluster &cluster) {
-    Cluster newClust(cluster);
-    return newClust;
-}
-
-Clustering::Cluster::~Cluster() {
-
-}
-
-void Clustering::Cluster::add(Clustering::PointPtr const &ptr) {
-    size+=1;
-    LNodePtr newPoint;
-    newPoint->p = ptr;
-    newPoint->next =nullptr;
-    LNodePtr current = points;
-
-    while (current != nullptr){
-        if (current->next == nullptr){
-            current->next = newPoint;
-        }
-        current = current->next;
+    Clustering::Cluster::Cluster(const Clustering::Cluster &cluster) {
+        size = cluster.getSize();
+        points = cluster.getPtr();
     }
-}
 
-Clustering::PointPtr const &Clustering::Cluster::remove(Clustering::PointPtr const &ptr) {
-    size-=1;
-    LNodePtr newPoint;
+    Clustering::Cluster &Clustering::Cluster::operator=(const Clustering::Cluster &cluster) {
+        Cluster *clustPtr = new Cluster;
+        Cluster newClust(cluster);
+        clustPtr = &newClust;
+        return *clustPtr;
 
-    LNodePtr current = points;
-
-    while (current != nullptr){
-        if (current->p == ptr){
-            return current->p;
-        }
-        current = current->next;
     }
-}
 
-Clustering::Cluster::Cluster(int sizee, Clustering::LNodePtr pt) {
-    size = sizee;
-    points = pt;
-}
+    Clustering::Cluster::~Cluster() {
 
-bool Clustering::Cluster::operator==(const Cluster &rhs) {
-    LNodePtr c1 = points;
-    LNodePtr c2 = rhs.getPtr();
-    bool status;
-
-    while (c1 != nullptr){
-        if (c1->p == c2->p){
-            status = true;
-        }
-        if (c1->p != c2->p){
-            return false;
-        }
-        c1=c1->next;
-        c2=c2->next;
     }
-}
+
+    void Clustering::Cluster::add(Clustering::PointPtr const &ptr) {
+
+        LNodePtr newPoint = new LNode;
+        newPoint->p = ptr;
+        newPoint->next = nullptr;
+        if (points == nullptr) {
+            points = newPoint;
+            size++;
+        }
+        LNodePtr current = points;
+
+        LNodePtr previous = nullptr;
+        bool inserted = false;
+
+
+
+        while (current != nullptr) {
+            if (*(current->p) > *ptr && inserted == false) {
+                newPoint->next = current;
+                previous->next = newPoint;
+                inserted = true;
+                size++;
+            }
+            previous = current;
+            current = current->next;
+
+        }
+        if (inserted==false){
+            previous->next=newPoint;
+            size++;}
+    }
+
+
+
+//must deallocate dynamic node
+    Clustering::PointPtr const &Clustering::Cluster::remove(Clustering::PointPtr const &ptr) {
+
+
+        LNodePtr current = points;
+
+        LNodePtr previous = nullptr;
+        while (current != nullptr) {
+            if (current->p == ptr) {
+                size--;
+                if (previous != nullptr) {
+                    previous->next = current->next;
+                }
+                else {
+                    points = points->next;
+                }
+                return current->p;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+
+    }
+
+    Clustering::Cluster::Cluster(int sizee, Clustering::LNodePtr pt) {
+        size = sizee;
+        points = pt;
+    }
+
+    bool Clustering::Cluster::operator==(const Cluster &rhs) {
+        LNodePtr c1 = points;
+        LNodePtr c2 = rhs.getPtr();
+        bool status;
+
+        while (c1 != nullptr) {
+            if (*(c1->p) == *(c2->p)) {
+                status = true;
+            }
+            if (*(c1->p) != *(c2->p)) {
+                return false;
+            }
+            c1 = c1->next;
+            c2 = c2->next;
+        }
+        return status;
+    }
 
 //appends second cluster to end of first
-Clustering::Cluster &Clustering::Cluster::operator+=(const Cluster &rhs){
-    LNodePtr current = points;
-    for (int i=0;i<size; i++){
+    Clustering::Cluster &Clustering::Cluster::operator+=(const Cluster &rhs) {
+        LNodePtr current = points;
+        for (int i = 0; i < size; i++) {
 
-        if (current->next == nullptr){
-            current->next = rhs.getPtr();
-        }
-        current = current->next;
-    }
-    return *this;
-};
-
-
-Clustering::Cluster &Clustering::Cluster::operator-=(const Cluster &rhs){
-    LNodePtr current = points;
-    LNodePtr previous;
-    LNodePtr currentrh = rhs.getPtr();
-
-    while (current != nullptr){
-        for (int i=0; i < rhs.getSize();i++){
-            if (current == currentrh) {
-                previous->next = current->next;
+            if (current->next == nullptr) {
+                current->next = rhs.getPtr();
             }
-            currentrh = currentrh->next;
-            }
-        previous = current;
-        current = current->next;
-    }
-
-
-    return *this;
-};
-
-Clustering::Cluster &Clustering::Cluster::operator+=(Point &rhs){
-    LNodePtr current = points;
-    LNodePtr newNode;
-    newNode->p = &rhs;
-    newNode->next = nullptr;
-    for (int i=0;i<size; i++){
-
-        if (current->next == nullptr){
-            current->next = newNode;
+            current = current->next;
         }
-        current = current->next;
+        size += rhs.getSize();
+        return *this;
+    };
+
+
+    Clustering::Cluster &Clustering::Cluster::operator-=(const Cluster &rhs) {
+        LNodePtr current = points;
+        LNodePtr previous = NULL;
+        LNodePtr currentrh;
+
+        for (int i = 0; i < size; i++) {
+            currentrh = rhs.getPtr();
+            std::cout << "U" << std::endl;
+            for (int j = 0; j < rhs.getSize(); j++) {
+
+                if (current->p == currentrh->p) {
+                    if (previous == NULL)
+                        points = current->next;
+                    else
+                        previous->next = current->next;
+
+                }
+
+                if (currentrh->next == nullptr)
+                    currentrh->next = rhs.getPtr();
+                else
+                    currentrh = currentrh->next;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        size -= rhs.getSize();
+        return *this;
+
     }
-    return *this;
-};
 
-Clustering::Cluster &Clustering::Cluster::operator-=( Point &rhs){
-    LNodePtr current = points;
-    LNodePtr previous;
+    Clustering::Cluster &Clustering::Cluster::operator+=(Point &rhs) {
+        LNodePtr current = points;
+        LNodePtr newNode = new LNode;
+        newNode->p = &rhs;
+        newNode->next = nullptr;
 
 
-    while (current != nullptr){
+        for (int i = 0; i <= size; i++) {
+
+            if (current->next == nullptr) {
+                current->next = newNode;
+
+            }
+            current = current->next;
+
+        }
+
+
+        size++;
+        return *this;
+
+    };
+
+    Clustering::Cluster &Clustering::Cluster::operator-=(Point &rhs) {
+        LNodePtr current = points;
+        LNodePtr previous;
+
+
+        while (current != nullptr) {
 
             if (current->p == &rhs) {
                 previous->next = current->next;
+                //mem leak
             }
 
-        previous = current;
-        current = current->next;
-    }
-
-
-    return *this;
-};
-
-const Clustering::Cluster operator+(const Clustering::Cluster &lhs, const Clustering::Cluster &rhs) {
-    Clustering::Cluster newClust((lhs.getSize() + rhs.getSize()),lhs.getPtr());
-    Clustering::LNodePtr current = newClust.getPtr();
-    for (int i=0;i<lhs.getSize(); i++){
-
-        if (current->next == nullptr){
-            current->next = rhs.getPtr();
+            previous = current;
+            current = current->next;
         }
-        current = current->next;
-    }
-    return newClust;
-}
 
-const Clustering::Cluster operator-(const Clustering::Cluster &lhs, const Clustering::Cluster &rhs){
-    Clustering::LNodePtr current = lhs.getPtr();
-    Clustering::LNodePtr previous;
-    Clustering::LNodePtr currentrh = rhs.getPtr();
-    int count = 0;
-    while (current != nullptr){
-        for (int i=0; i < rhs.getSize();i++){
-            if (current == currentrh) {
-                previous->next = current->next;
-                count++;
+        size--;
+        return *this;
+    };
+
+    Clustering::Cluster const operator+(Clustering::Cluster const &lhs, Clustering::Cluster const &rhs) {
+        Clustering::LNodePtr current = lhs.getPtr();
+        Clustering::LNodePtr head = lhs.getPtr();
+        Clustering::LNodePtr currentrh=rhs.getPtr();
+        Cluster plusClust(lhs);
+
+
+
+        bool add = true;  //indicator for function completion
+
+        while (currentrh != nullptr) {        //while not the end of rhs list
+            add = true;
+            if (current == nullptr)
+                current = lhs.getPtr();
+            while (current != nullptr) {//while not the end of the lhs list
+
+                if (currentrh->p == current->p) {       //if address of pointer1 is address of pointer2
+                    add = false;                        //change indicator
+                }
+
+                current = current->next;
+
+            }
+            if (add == true) {                      //if indicator unchanged
+                plusClust.add(currentrh->p);
+
+            }
+        currentrh = currentrh->next;
+    }
+        Clustering::LNodePtr printer = plusClust.getPtr();
+        for (int i=0;i< plusClust.getSize();i++) {
+            std::cout << *printer->p << std::endl;
+            printer=printer->next;
+        }
+    return plusClust;
+
+    }
+
+
+    const Clustering::Cluster operator-(const Clustering::Cluster &lhs, const Clustering::Cluster &rhs) {
+        Clustering::LNodePtr current = lhs.getPtr();
+        Clustering::LNodePtr currentrh = rhs.getPtr();
+        Clustering::Cluster minusClust(lhs);
+
+        while (currentrh != nullptr) {
+            std::cout << "hi" << std::endl;
+            current = lhs.getPtr();
+            for (int i = 0; i < lhs.getSize(); i++) {
+                if (current->p == currentrh->p) {
+                    minusClust.remove(currentrh->p);
+                }
+                current = current->next;
+
             }
             currentrh = currentrh->next;
         }
-        previous = current;
-        current = current->next;
+
+        return minusClust;
+    };
+
+    const Clustering::Cluster operator+(const Clustering::Cluster &lhs, const Clustering::PointPtr &rhs) {
+        Clustering::Cluster newClust(lhs);
+        Clustering::LNodePtr current = lhs.getPtr();
+        bool status = false;
+        for (int i = 0;i<lhs.getSize();i++){
+            if (current->p == rhs)
+                status = true;
+        };
+        if (status == false)
+            newClust.add(rhs);
+
+        return newClust;
     }
-    Clustering::Cluster newClust(lhs.getSize()-count,lhs.getPtr());
 
-    return newClust;
-};
+    const Clustering::Cluster operator-(const Clustering::Cluster &lhs, const Clustering::PointPtr &rhs) {
+        Clustering::Cluster newClust(lhs);
+        Clustering::LNodePtr current = lhs.getPtr();
+        bool status = false;
+        for (int i = 0;i<lhs.getSize();i++){
+            if (current->p == rhs)
+                status = true;
+            current=current->next;
+        };
+        if (status == true)
+            newClust.remove(rhs);
 
-const Clustering::Cluster operator+(const Clustering::Cluster &lhs, const Clustering::PointPtr &rhs){
-    Clustering::Cluster newClust(lhs.getSize() + 1,lhs.getPtr());
-    Clustering::LNodePtr current = newClust.getPtr();
-    Clustering::LNodePtr newNode;
-    newNode->p = rhs;
-    newNode->next = nullptr;
-    for (int i=0;i<lhs.getSize(); i++){
+        return newClust;
+    }
 
-        if (current->next == nullptr){
-            current->next = newNode;
+    std::ostream &operator<<(std::ostream &ostream, const Cluster &clust){
+        Clustering::LNodePtr current = clust.getPtr();
+
+        for (int i=0;i< clust.getSize();i++) {
+            ostream << "Point #" << i+1 << "\n" << *current->p << "\n";
+            current=current->next;
         }
-        current = current->next;
+
     }
-    return newClust;
-}
-
-const Clustering::Cluster operator-(const Clustering::Cluster &lhs, const Clustering::PointPtr &rhs){
-    Clustering::LNodePtr current = lhs.getPtr();
-    Clustering::LNodePtr previous;
-    Clustering::Cluster newClust(lhs.getSize()-1,lhs.getPtr());
-
-
-    while (current != nullptr) {
-        if (current->p == rhs) {
-            previous->next = current->next;
-        }
-
-
-        previous = current;
-        current = current->next;
-    }
-
-
-
-    return newClust;
 }
